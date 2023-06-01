bf76c1ecfcd72ed3dcc2693fbbe2d381fa27aab0
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2023-06-01 15:28:11 +0900

    key 삭제

---
+++ 230601_class_AWS_domain_EC2/app.js
@@ -0,0 +1,38 @@
+
+
+// 오늘은 도메인 붙이고, https 설정까지, 진행할 예정
+    // Nginx 
+
+// 도메인 설정 좋은 점 
+    // 연락이 진짜 많이 옴 
+    // 면접 연락 정도가 많이 다름 ⭐⭐⭐
+
+
+// 노드 설치 
+// nvm 노드 버전 매니저 
+    // node.js 설치 하고, 다른 버전으로 설치 할 때, 
+    // 삭제하고, 다시 설치할 필요 없이, 버전관리가 편하다. 
+    // 원하는 버전을 설치 받고, 바로 스위치 가능 
+
+    // 첫 번째 설치 
+        // curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
+    
+    // 두 번째 설치
+        // curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash 
+
+    // 소스 파일 적용
+    // source ~/.bashrc
+
+    // 전체 목록 확인
+    // nvm list-remote
+
+    // 원하는 버전을 선택하고 설치 
+    // nvm install 16(여기서 숫자 버전)
+
+    
+    // 노드 모듈 설치 
+        // npm i 
+        // npm start 
+
+
+// 본격 필기는 옵시에 함 
