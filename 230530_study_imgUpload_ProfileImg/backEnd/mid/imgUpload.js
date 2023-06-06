6a8f4171055125b55515f78db07e5c317aa7837a
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2023-06-06 23:36:27 +0900

    게시판 CRUD 중 CREATE 의 POST 구현 중

--- 230530_study_imgUpload_ProfileImg/backEnd/mid/imgUpload.js
+++ 230530_study_imgUpload_ProfileImg/backEnd/mid/imgUpload.js
@@ -2,13 +2,14 @@
 const multer = require("multer");
 
 const path = require("path");
+    // path 를 왜 가져오지 ❓❓❓❓❓❓❓❓❓❓❓
 
 
 // 'multer 함수' 안에, 매개변수로, 객체 형태의 인자를 전달 
     // storage 속성을 통해서, 업로드 된 파일을, 어디에 저장시킬지 지정 할 수 있다. 
 exports.Upload = multer({
     
-    // '어떤 폴더' 에 '어떤 파일' 을 저장할지 
+    // 1. '어떤 폴더' 에 '어떤 파일' 을 저장할지 
     storage : multer.diskStorage({
         // diskStorage 기능 
             // 서버 컴퓨터의 하드 디스크에 업로드 파일을 저장
@@ -26,6 +27,9 @@ exports.Upload = multer({
                 filename : (req, file, done) => {
                     // 확장자 추출
                     const ext = path.extname(file.originalname)
+                        // [해석]
+                            // path 모듈 안에 있는 extname 메소드를 사용해서, 전달받은 file 의 original name 에서 확장자만을 추출
+
                         // path.extname 기능
                             // '파일 경로' 를 매개변수로 받아서 > '확장자' 를 추출 ex) png, jpg 등
                         // file.originalname
@@ -36,7 +40,9 @@ exports.Upload = multer({
                     // 파일 이름이 겹치지 않게, 현재 시간 값 추가 
                     const filename = path.basename(file.originalname , ext) + "_" + Date.now() + ext;
                         // [해석]
-                            // basename 기능 
+                            // basename 메소드
+                                // path 모듈 안에 있는 기능임. 
+                                // 전달받은 file 의 원본 이름이 있고, 거기에, 확장자를 '추가' 하게된다. 
                                 // 확장자를 '추가' 및 '제거' 할 수 있다. ex) 1.js => 1
                                 // 첫 번째 매개변수 : 파일의 경로 
                                 // 두 번째 매개변수 : 옵션
@@ -50,7 +56,7 @@ exports.Upload = multer({
                 }
     }), 
 
-    // 파일 사이즈 설정 (최대 파일 사이즈 설정 )
+    // 2. 파일 사이즈 설정 (최대 파일 사이즈 설정 )
     limits : {fileSize : 5 * 1024 * 1024}       // 5mb 임 
 
 })
